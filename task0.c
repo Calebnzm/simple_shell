7002518abf71e736434248a069251e18ab9ab0b5
kelvinthuo999 <thuokelvinngigi@gmail.com>
2023-07-20 01:11:20 -0700

    added write function

--- task0.c
+++ task0.c
@@ -1,29 +1,36 @@
 #include "shell.h"
 
 /**
- * add - Calculate sum
- * Return: sum(success), -1(fail)
+ * add - Calculate sum.
+ * Return: Sum (success), -1 (fail).
  */
-
 int add(void)
 {
 	int a, b, sum;
+	char buffer[100];
+	int n;
 
 	srand(time(0));
 	a = rand() % 100 + 1;
 	b = rand() % 200 + 101;
 
-	printf("a = %d \nb = %d\n", a, b);
+	/* Use the write function to print the values of a and b */
+	n = sprintf(buffer, "a = %d\nb = %d\n", a, b);
+	write(STDOUT_FILENO, buffer, n);
+
 	sum = a + b;
-	printf("%d\n", sum);
+
+	/* Use the write function to print the sum */
+	n = sprintf(buffer, "%d\n", sum);
+	write(STDOUT_FILENO, buffer, n);
+
 	return (sum);
 }
 
 /**
- * main - add random numbers
- * Return: 0(always)
+ * main - Add random numbers.
+ * Return: Always 0.
  */
-
 int main(void)
 {
 	add();
