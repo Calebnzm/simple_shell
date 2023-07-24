015e98b53ab13217564832c2963997f0ceeb2c54
kelvinthuo999 <thuokelvinngigi@gmail.com>
2023-07-23 23:37:52 -0700

    created files to handle auxillary functions

--- shell.h
+++ shell.h
@@ -2,10 +2,23 @@
 #define SHELL_H
 
 #include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <dirent.h>
 #include <stdlib.h>
 #include <time.h>
 #include <unistd.h>
 
+#define MAX_COMMAND_LENGTH 100
+#define BUFFER_SIZE 1024
+
 int add();
+void display_prompt();
+void execute_command(char *command_with_args, char **env);
+char *find_command_path(char *command, char **env);
+int custom_strlen(const char *str);
+int is_valid_command(char *command, char **env);
 
 #endif /* KELVIN && CALEB */
