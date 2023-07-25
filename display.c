c1f87f9ae74fd36b99c0563e82cb2bc7f8d3f1da
root <calebnzioka2027@gmail.com>
2023-07-25 09:21:01 -0700

    Display Betty

--- display.c
+++ display.c
@@ -1,12 +1,19 @@
 #include "shell.h"
 
 /**
-* display_prompt -  Function to display the shell prompt
-*
-* Return: nothing
-*/
+ * display_prompt - Function to display the shell prompt
+ *
+ * This function is responsible for displaying the shell prompt, which is
+ * typically represented by "$ " in most shells.
+ *
+ * Return: Nothing
+ */
 void display_prompt(void)
 {
+	/* The prompt to be displayed */
 	char prompt[] = "$ ";
+
+	/* Write the prompt to the standard output */
 	write(STDOUT_FILENO, prompt, sizeof(prompt) - 1);
 }
+
