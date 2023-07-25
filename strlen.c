ac59d588c9c9ca9fc0e3da5805661508fa5f72fa
root <calebnzioka2027@gmail.com>
2023-07-25 09:25:07 -0700

    Strlen Betty

--- strlen.c
+++ strlen.c
@@ -1,17 +1,23 @@
 #include "shell.h"
 
 /**
-* custom_strlen - calculates length of string
-* @str - the string
-*
-* Return: lenght of string(success)
-*/
+ * custom_strlen - Calculates the length of a string
+ * @str: The input string
+ *
+ * This function calculates the length of the given string by iterating
+ * through each character until it encounters the null terminator.
+ *
+ * Return: The length of the string (success).
+ */
 int custom_strlen(const char *str)
 {
-int len = 0;
-while (str[len] != '\0')
-{
-len++;
+	int len = 0;
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+
+	return (len);
 }
-return (len);
-}
+
