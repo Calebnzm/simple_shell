e96a87eac7e7ab62ea95b3dd9c0ae9027f7e88d4
kelvinthuo999 <thuokelvinngigi@gmail.com>
2023-07-26 03:04:57 -0700

    main betty checker

--- main.c
+++ main.c
@@ -1,25 +1,43 @@
 #include "shell.h"
 
+/**
+ * main - Entry point for the shell program.
+ *
+ * Description: This function initializes the shell and enters a loop
+ * where it continuously displays a prompt, reads input from the user,
+ * and executes the entered command.
+ *
+ * Return: Always returns 0 to indicate successful program termination.
+ */
 int main(void)
 {
-    char buffer[BUFFER_SIZE];
-    int read_bytes;
-    while (1)
-    {
-        display_prompt();
-        if ((read_bytes = read(STDIN_FILENO, buffer, BUFFER_SIZE)) == -1)
-        {
-            perror("Error reading input");
-            continue;
-        }
-        if (read_bytes == 0)
-        {
-            printf("\n");
-            break;
-        }
-        buffer[read_bytes - 1] = '\0';
-        execute_command(buffer);
-    }
-    return 0;
-}
+	/* Declare variables */
+	char buffer[BUFFER_SIZE]; /* Buffer to store user input */
+	int read_bytes; /* Number of bytes read from user input */
 
+	/* Start the main loop */
+	while (1)
+	{
+		display_prompt(); /* Display the shell prompt */
+
+		/* Read input from the user and handle errors */
+		read_bytes = read(STDIN_FILENO, buffer, BUFFER_SIZE);
+		if (read_bytes == -1)
+		{
+			perror("Error reading input");
+			continue; /* Continue to the next iteration of the loop */
+		}
+
+		/* Check for end of input (Ctrl+D) and break the loop */
+		if (read_bytes == 0)
+		{
+			printf("\n"); /* Print a new line to keep the output clean */
+			break; /* Exit the loop */
+		}
+
+		buffer[read_bytes - 1] = '\0'; /* Null-terminate the input string */
+		execute_command(buffer); /* Execute the command entered by the user */
+	}
+
+	return (0); /* Indicate successful program termination */
+}
