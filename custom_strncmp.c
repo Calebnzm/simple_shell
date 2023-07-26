d18c0f20000d04d59f8d92c7a68002b2af443e0e
kelvinthuo999 <thuokelvinngigi@gmail.com>
2023-07-26 13:34:39 -0700

    removed unwanted functions

--- custom_strncmp.c
+++ custom_strncmp.c
@@ -6,16 +6,19 @@
  * @s2: The second string
  * @n: The maximum number of characters to compare
  *
- * Return: 0 if strings are equal up to n characters, negative value if s1 < s2,
+ * Return: 0 if strings are equal up to n characters,
+ * negative value if s1 < s2,
  *         positive value if s1 > s2
  */
 int custom_strncmp(const char *s1, const char *s2, size_t n)
 {
-    for (size_t i = 0; i < n; i++)
-    {
-        // Check for the end of the strings or if characters don't match
-        if (s1[i] == '\0' || s1[i] != s2[i])
-            return (unsigned char)s1[i] - (unsigned char)s2[i];
-    }
-    return 0; // Both strings are equal up to n characters
+	size_t i;
+
+	for (i = 0; i < n; i++)
+	{
+	/* Check for the end of the strings or if characters don't match */
+		if (s1[i] == '\0' || s1[i] != s2[i])
+		return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+	}
+	return (0); /* Both strings are equal up to n characters */
 }
